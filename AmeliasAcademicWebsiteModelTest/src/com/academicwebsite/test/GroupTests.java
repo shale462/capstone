55ae4f3e9e4de3d13f2562fccbf79bea5da4eae0
Elizabeth Zinger <ezinger-cc@conestogac.on.ca>
2015-07-15 16:31:29 -0400

    Created empty Tests for my classes

---
+++ AmeliasAcademicWebsiteModelTest/src/com/academicwebsite/test/GroupTests.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.academicwebsite.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Liz
+ *
+ */
+public class GroupTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#getGroupID()}.
+	 */
+	@Test
+	public void testGetGroupID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#setGroupID(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetGroupID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#getGroupName()}.
+	 */
+	@Test
+	public void testGetGroupName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#setGroupName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetGroupName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#getUsers()}.
+	 */
+	@Test
+	public void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#setUsers(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testSetUsers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#Group()}.
+	 */
+	@Test
+	public void testGroup() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Group#Group(java.lang.Integer, java.lang.String)}.
+	 */
+	@Test
+	public void testGroupIntegerString() {
+		fail("Not yet implemented");
+	}
+
+}
