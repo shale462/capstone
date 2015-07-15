cb713a11d170716ce5791575beaaf546b9754f24
Sarah <Sarah@10.192.116.3>
2015-07-15 15:38:09 -0400

    added test case package

---
+++ AmeliasAcademicWebsiteModelTest/src/com/academicwebsite/test/SurveyTests.java
@@ -0,0 +1,152 @@
+/**
+ * 
+ */
+package com.academicwebsite.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sarah
+ *
+ */
+public class SurveyTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getSurveyID()}.
+	 */
+	@Test
+	public void testGetSurveyID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setSurveyID(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetSurveyID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getTitle()}.
+	 */
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setTitle(java.lang.String)}.
+	 */
+	@Test
+	public void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getDateCreated()}.
+	 */
+	@Test
+	public void testGetDateCreated() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setDateCreated(java.util.Date)}.
+	 */
+	@Test
+	public void testSetDateCreated() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getIsActive()}.
+	 */
+	@Test
+	public void testGetIsActive() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setIsActive(java.lang.Boolean)}.
+	 */
+	@Test
+	public void testSetIsActive() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getIsPublic()}.
+	 */
+	@Test
+	public void testGetIsPublic() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setIsPublic(java.lang.Boolean)}.
+	 */
+	@Test
+	public void testSetIsPublic() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#getQuestions()}.
+	 */
+	@Test
+	public void testGetQuestions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#setQuestions(java.util.List)}.
+	 */
+	@Test
+	public void testSetQuestions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#Survey()}.
+	 */
+	@Test
+	public void testSurvey() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.academicwebsite.model.Survey#Survey(int, java.lang.String, java.lang.String, java.util.List)}.
+	 */
+	@Test
+	public void testSurveyIntStringStringListOfQuestion() {
+		fail("Not yet implemented");
+	}
+
+}
