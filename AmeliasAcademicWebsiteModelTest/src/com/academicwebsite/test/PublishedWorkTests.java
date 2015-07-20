9c268e07378f285186797370ed2ff7bf49a40e02
Sarah <Sarah@10.192.113.234>
2015-07-20 16:03:38 -0400

    published work tests

--- AmeliasAcademicWebsiteModelTest/src/com/academicwebsite/test/PublishedWorkTests.java
+++ AmeliasAcademicWebsiteModelTest/src/com/academicwebsite/test/PublishedWorkTests.java
@@ -2,83 +2,105 @@ package com.academicwebsite.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.academicwebsite.model.PublishedWork;
+
 public class PublishedWorkTests {
+	PublishedWork publishedWork;
+	String fileLocation;
+	Integer pubWorkID;
 
 	@Before
 	public void setUp() throws Exception {
+		publishedWork = new PublishedWork();
 	}
 
 	@Test
 	public void testGetPubWorkID() {
-		
+		assertSame(0, publishedWork.getPubWorkID());
 	}
 
 	@Test
 	public void testSetPubWorkID() {
-		fail("Not yet implemented");
+		pubWorkID = 1;
+		publishedWork.setPubWorkID(pubWorkID);
+		assertEquals(pubWorkID, publishedWork.getPubWorkID());
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertNull(publishedWork.getTitle());
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		String title = "Published Work Title";
+		publishedWork.setTitle(title);
+		assertEquals(title, publishedWork.getTitle());
 	}
 
 	@Test
 	public void testGetAuthor() {
-		fail("Not yet implemented");
+		assertNull(publishedWork.getAuthor());
 	}
 
 	@Test
 	public void testSetAuthor() {
-		fail("Not yet implemented");
+		String author = "author";
+		publishedWork.setAuthor(author);
+		assertEquals(author, publishedWork.getAuthor());
 	}
 
 	@Test
 	public void testGetFileLocation() {
-		fail("Not yet implemented");
+		assertSame("", publishedWork.getFileLocation());
 	}
 
 	@Test
 	public void testSetFileLocation() {
-		fail("Not yet implemented");
+		fileLocation = "C:\\user\\documents";
+		publishedWork.setFileLocation(fileLocation);
+		assertEquals(fileLocation, publishedWork.getFileLocation());
 	}
 
 	@Test
 	public void testGetDateCreated() {
-		fail("Not yet implemented");
+		assertNull(publishedWork.getDateCreated());
 	}
 
 	@Test
 	public void testSetDateCreated() {
-		fail("Not yet implemented");
+		Date dateCreated = new Date();
+		publishedWork.setDateCreated(dateCreated);
+		assertEquals(dateCreated, publishedWork.getDateCreated());
 	}
 
 	@Test
 	public void testGetDatePosted() {
-		fail("Not yet implemented");
+		assertNull(publishedWork.getDatePosted());
 	}
 
 	@Test
 	public void testSetDatePosted() {
-		fail("Not yet implemented");
+		Date datePosted = new Date();
+		publishedWork.setDatePosted(datePosted);
+		assertEquals(datePosted, publishedWork.getDatePosted());
 	}
 
 	@Test
 	public void testPublishedWork() {
-		fail("Not yet implemented");
+		PublishedWork pubWork = new PublishedWork();
+		assertNotNull(pubWork);
 	}
 
 	@Test
 	public void testPublishedWorkIntegerString() {
-		fail("Not yet implemented");
+		PublishedWork pubWork = new PublishedWork(pubWorkID, fileLocation);
+		assertNotNull(pubWork);
 	}
 
 }
