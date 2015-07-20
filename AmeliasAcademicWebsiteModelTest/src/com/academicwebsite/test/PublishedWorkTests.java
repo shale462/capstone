a13c5a2c62c504b06d28c8b48d5a9bdb3fd6f763
Sarah <Sarah@10.192.113.234>
2015-07-20 15:48:16 -0400

    test

---
+++ AmeliasAcademicWebsiteModelTest/src/com/academicwebsite/test/PublishedWorkTests.java
@@ -0,0 +1,84 @@
+package com.academicwebsite.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PublishedWorkTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetPubWorkID() {
+		
+	}
+
+	@Test
+	public void testSetPubWorkID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFileLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateCreated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDateCreated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDatePosted() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDatePosted() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPublishedWork() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPublishedWorkIntegerString() {
+		fail("Not yet implemented");
+	}
+
+}
