4a0b55c0330c0242876ecef9eced7bf870c4c6b8
Elizabeth Zinger <ezinger-cc@conestogac.on.ca>
2015-07-20 15:44:05 -0400

    work on the adminDAOImpl.java

--- AmeliasAcademicWebsiteModel/src/com/academicwebsite/dao/impl/adminDAOImpl.java
+++ AmeliasAcademicWebsiteModel/src/com/academicwebsite/dao/impl/adminDAOImpl.java
@@ -1,15 +1,84 @@
 package com.academicwebsite.dao.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.academicwebsite.dao.adminDAO;
 import com.academicwebsite.model.Admin;
 import com.academicwebsite.model.Group;
 import com.academicwebsite.model.PublishedWork;
 import com.academicwebsite.model.Report;
 
-public class adminDAOImpl {
+public class adminDAOImpl implements adminDAO {
 
 	private static adminDAOImpl dao;
 
+	private adminDAOImpl() {
+	}
+
+	public static adminDAOImpl getAdminDAO() {
+		if (dao == null) {
+			dao = new adminDAOImpl();
+		}
+		return dao;
+	}
+
+	private Connection openConnection() {
+		Connection connection = null;
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/ameliadatabase;create=true");
+
+			// connection =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/ameliadatabase",
+			// "root",
+			// "Zayra*2012");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	private void closeConnection(Connection connection) {
+		if (connection != null) {
+
+			try {
+				connection.close();
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public void addGroup(Group group) {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = openConnection();
+			String query = "INSERT INTO ameliadatabase.ADMIN values(?,?)";
+			ps = con.prepareStatement(query);
+			ps.setString(1, group.getGroupName());
+			ps.setString(2, group.getDescription());
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+
+			closeConnection(con);
+		}
 
 	}
 
