100a636edb2b46715d05f01b6502da4d4a1ef1df
Elizabeth Zinger <ezinger-cc@conestogac.on.ca>
2015-07-20 14:12:25 -0400

    Seeing if database pushes

---
+++ AmeliasAcademicWebsiteModel/src/DBTest.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBTest {
+
+	public static void main(String[] args) throws Exception {
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/ameliadatabase", "root",
+				"Zayra*2012");
+
+		PreparedStatement statement = con
+				.prepareStatement("select * from admin");
+
+		ResultSet result = statement.executeQuery();
+
+		while (result.next()) {
+
+			System.out.println(result.getString(1) + result.getString(2));
+		}
+
+	}
+
+}
---
+++ AmeliasAcademicWebsiteModel/src/com/academicwebsite/dao/impl/adminDAOImpl.java
@@ -0,0 +1,72 @@
+package com.academicwebsite.dao.impl;
+
+import com.academicwebsite.model.Admin;
+import com.academicwebsite.model.Group;
+import com.academicwebsite.model.PublishedWork;
+import com.academicwebsite.model.Report;
+
+public class adminDAOImpl {
+
+	private static adminDAOImpl dao;
+
+	public void addGroup(Group group) {
+
+	}
+
+	public void editGroup(int groupID) {
+
+	}
+
+	public void deleteGroup(int groupID) {
+
+	}
+
+	public void addUserToGroup(int groupID, int userID) {
+
+	}
+
+	public void removeUserFromGroup(int groupID, int userID) {
+
+	}
+
+	public void addPublishedWork(PublishedWork publishedWork) {
+
+	}
+
+	public void editPublishedWork(int pubWorkID) {
+
+	}
+
+	public void deletePublishedWork(int pubWorkID) {
+
+	}
+
+	public void addAdmin(Admin admin) {
+
+	}
+
+	public void editAdmin(int adminID) {
+
+	}
+
+	public void deleteAdmin(int adminID) {
+
+	}
+
+	public void addReport(Report report) {
+
+	}
+
+	public void editReport(int reportID) {
+
+	}
+
+	public void deleteReport(int reportID) {
+
+	}
+
+	public void exportReport(int reportID) {
+
+	}
+
+}
